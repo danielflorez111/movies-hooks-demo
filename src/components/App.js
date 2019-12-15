b71d2501b67aff6701896d0880889e2a4285a325
Daniel Florez <danielflorez111@hotmail.com>
2019-12-15 13:21:45 -0500

    Basic components Header, Search and Movie

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
+import Movie from './Movie/Movie';
+import Search from './Search/Search';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>App</h1>
+
+      <Header />
+      <Movie />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Movie.css';
+
+function Movie() {
+    return (
+        <h1>Movie</h1>
+    );
+}
+
+export default Movie;
